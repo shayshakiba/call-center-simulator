49a2a9d0a9ea425ddb2f145c6c3896c72adb9588
Shayan Shakiba <shayshakiba@gmail.com>
2023-10-12 15:22:55 +0330

    Format simulator.py

--- simulator.py
+++ simulator.py
@@ -1,8 +1,8 @@
 import sys
-from time import sleep
-from threading import Thread
-from random import choice
 from queue import Queue
+from random import choice
+from threading import Thread
+from time import sleep
 
 
 class Operator(Thread):
@@ -23,13 +23,14 @@ class Operator(Thread):
 
     def handle_caller(self, caller):
         print(
-            f'{self.name} answered a call from {caller.name} categorized as {caller.call_category}...')
+            f"{self.name} answered a call from {caller.name} categorized as {caller.call_category}..."
+        )
 
-        if caller.call_category == 'customer_service':
+        if caller.call_category == "customer_service":
             call_duration = 0.3
-        elif caller.call_category == 'technical_support':
+        elif caller.call_category == "technical_support":
             call_duration = 0.5
-        elif caller.call_category == 'sales_support':
+        elif caller.call_category == "sales_support":
             call_duration = 0.2
 
         if caller.has_weak_signal:
@@ -38,7 +39,8 @@ class Operator(Thread):
         sleep(call_duration)
 
         print(
-            f'{self.name} finished talking to {caller.name} after {round(call_duration, 1)} second(s).')
+            f"{self.name} finished talking to {caller.name} after {round(call_duration, 1)} second(s)."
+        )
 
         self.work_duration += call_duration
 
@@ -46,7 +48,7 @@ class Operator(Thread):
         caller.start()
 
     def take_a_break(self):
-        print(f'{self.name} took a break...')
+        print(f"{self.name} took a break...")
 
         break_duration = 0.1
         if self.need_extended_break():
@@ -56,7 +58,7 @@ class Operator(Thread):
 
         sleep(break_duration)
 
-        print(f'{self.name} returned after {round(break_duration, 1)} second(s).')
+        print(f"{self.name} returned after {round(break_duration, 1)} second(s).")
 
     def need_extended_break(self):
         return self.work_duration > 1.0
@@ -76,21 +78,21 @@ class Caller(Thread):
         if self.want_to_send_feedback():
             self.send_feedback()
 
-        print(f'{self.name} ended the call.')
+        print(f"{self.name} ended the call.")
 
     def fill_survey(self):
-        print(f'{self.name} started filling survey...')
+        print(f"{self.name} started filling survey...")
 
         sleep(0.1)
 
-        print(f'{self.name} finished filling survey.')
+        print(f"{self.name} finished filling survey.")
 
     def send_feedback(self):
-        print(f'{self.name} started sending feedback...')
+        print(f"{self.name} started sending feedback...")
 
         sleep(0.2)
 
-        print(f'{self.name} finished sending feedback.')
+        print(f"{self.name} finished sending feedback.")
 
     def want_to_send_feedback(self):
         return choice([True, False])
@@ -100,7 +102,7 @@ def initialize_operators(input_file):
     number_of_operators = int(input_file.readline().strip())
 
     for operator_index in range(1, number_of_operators + 1):
-        operators.append(Operator(f'Operator {operator_index}'))
+        operators.append(Operator(f"Operator {operator_index}"))
 
 
 def initialize_callers(input_file):
@@ -111,7 +113,7 @@ def initialize_callers(input_file):
         name, call_category = caller_info[0:2]
 
         # checks if the caller has a weak signal
-        has_weak_signal = 'weak_signal' in caller_info[2:]
+        has_weak_signal = "weak_signal" in caller_info[2:]
 
         callers.put(Caller(name, call_category, has_weak_signal))
 
@@ -122,14 +124,14 @@ def start_operators():
 
 
 def simulate_call_center(input_file_path):
-    with open(input_file_path, 'r') as input_file:
+    with open(input_file_path, "r") as input_file:
         initialize_operators(input_file)
         initialize_callers(input_file)
 
     start_operators()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     global operators, callers
 
     operators = []
